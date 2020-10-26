e86137bedb2abeec1661f00eaa81a6c2b7832369
Arseny Yusufov <senya.yusufov@yandex.ru>
2020-10-26 23:51:39 +0300

    implement hw4

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,16 @@ import java.io.File
 import java.lang.Exception
 
 fun main(args: Array<String>) {
-    val (size, listOfQueries) = askInput()
-    println(applyAlgo(listOfQueries, size, ::fifo))
-    println(applyAlgo(listOfQueries, size, ::lru))
-    println(applyAlgo(listOfQueries, size, ::opt))
+//    val (size, listOfQueries) = askInput()
+
+//    val size = 5
+//    val listOfQueries = randomInput(10..15, 1..20)
+//    println(listOfQueries)
+//    println(applyAlgo(listOfQueries, size, ::fifo))
+//    println(applyAlgo(listOfQueries, size, ::lru))
+//    println(applyAlgo(listOfQueries, size, ::opt))
+
+    applyAlgoDynamic(numberOfQueries = 50, memorySize = 3, depth = 5, algo = ::opt)
 }
 
 fun askInput(): Pair<Int, List<Int>> {
@@ -60,8 +66,10 @@ fun randomInput(sizeRange: IntRange, queryRange: IntRange): List<Int> {
 // Apply algo uses one of algorithms on every step like it described in README.md.
 // It gives scalability to implement new algorithms in future. We can even use different algorithms on different queries.
 
+typealias Algo = (restQueries: List<Int>, loadedPages: MutableList<Int>) -> Pair<Int, MutableList<Int>>
+
 fun applyAlgo(listOfQueries: List<Int>, memorySize: Int,
-              algo: (restQueries: List<Int>, loadedPages: MutableList<Int>) -> Pair<Int, MutableList<Int>>): Pair<Int, MutableList<Int>> {
+              algo: Algo): Pair<Int, MutableList<Int>> {
     val listOfAnswers: MutableList<Int> = mutableListOf<Int>()
     var loadedPages: MutableList<Int> = mutableListOf<Int>()
 
@@ -99,6 +107,54 @@ fun applyAlgo(listOfQueries: List<Int>, memorySize: Int,
     }
 
     return Pair(counter, listOfAnswers)
+}
+
+fun getRandomQuery(): Int {
+    return (1..10).random()
+}
+
+fun applyAlgoDynamic(algo: Algo, memorySize: Int = 1, depth: Int = 1, numberOfQueries: Int = 1) {
+    require(depth in 1 until numberOfQueries) { "Depth can not be less than 1 and more than number of queries" }
+    require(numberOfQueries > 0) { "Number of queries can not be less than 1" }
+    require(memorySize > 0) { "Memory size can not be less than 1" }
+
+    var loadedPages: MutableList<Int> = mutableListOf<Int>()
+
+    var restQueries = mutableListOf<Int>()
+    var queriesDone: Int = 0
+
+    repeat(memorySize) {
+        val queryPage = getRandomQuery()
+        queriesDone++
+        loadedPages.add(queryPage)
+        println(-2)
+    }
+
+    repeat(depth - memorySize) {
+        val queryPage = getRandomQuery()
+        queriesDone++
+        restQueries.add(queryPage)
+    }
+
+    while (queriesDone != numberOfQueries) {
+        val query = getRandomQuery()
+        queriesDone++
+        restQueries.add(query)
+//        Uncomment if you want to check work
+//        println("called with restQueries: $restQueries, loadedPages: $loadedPages")
+        val algoResult = algo(restQueries, loadedPages)
+        loadedPages = algoResult.second
+
+        if (algoResult.first != -1) {
+            loadedPages.remove(algoResult.first)
+            loadedPages.add(restQueries.first())
+        }
+
+        println(algoResult.first)
+
+        restQueries = restQueries.slice(1 until restQueries.size).toMutableList()
+    }
+
 }
 
 // Algorithms
